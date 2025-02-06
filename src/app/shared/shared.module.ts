a192413cc8366008043ab7d42ab14ec1a931f314
Tales <taleskodama@gmail.com>
2025-02-05 21:54:26 -0300

    criação de poke-list e poke-search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 //Components
 
@@ -10,9 +12,13 @@ import { PokeHeaderComponent } from './poke-header/poke-header.component';
 @NgModule({
   declarations: [
     PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent,
   ],
   exports: [
     PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent,
   ],
   imports: [
     CommonModule
